5b4dfecc118e51a839e44ae9e90867081fef7b2b
Andrew <andrewmoren93@gmail.com>
2023-09-20 06:26:25 +0300

    need fix images

--- app.js
+++ app.js
@@ -41,20 +41,20 @@ function matchCards(img1, img2) {
     disableDeck = false;
   }, 1200);
 }
-function shuffleCard() {
-  matched = 0;
-  disableDeck = false;
-  cardOne = cardTwo = "";
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
-  cards.forEach((card, i) => {
-    card.classList.remove("flip");
-    let imgTag = card.querySelector(".back-view img");
-    imgTag.src = `images/img-${arr[i]}.png`;
-    card.addEventListener("click", flipCard);
-  });
-}
-shuffleCard();
+// function shuffleCard() {
+//   matched = 0;
+//   disableDeck = false;
+//   cardOne = cardTwo = "";
+//   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+//   arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+//   cards.forEach((card, i) => {
+//     card.classList.remove("flip");
+//     let imgTag = card.querySelector(".back-view img");
+//     imgTag.src = `images/img-${arr[i]}.png`;
+//     card.addEventListener("click", flipCard);
+//   });
+// }
+// shuffleCard();
 
 cards.forEach((card) => {
   card.addEventListener("click", flipCard);
